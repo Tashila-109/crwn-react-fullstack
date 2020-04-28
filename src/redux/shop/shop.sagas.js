2d1a0b87cb1d5c860f9a2bed1ce7f5873852e6d4
Tashila <starbuks1298@gmail.com>
2020-04-28 23:28:59 +0530

    Added Root Saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put } from 'redux-saga/effects';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
@@ -7,8 +7,6 @@ import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-	yield console.log('Iam Fired');
-
 	try {
 		const collectionRef = firestore.collection('collections');
 		const snapshot = yield collectionRef.get();
@@ -20,5 +18,5 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-	yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+	yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
 }
